ca96f478e902b86e04f5630c4c23e27b52a3c3a8
Alex Reyes <alextreyes11@gmail.com>
2023-08-26 19:17:21 -0400

    first

---
+++ app.js
@@ -0,0 +1,16 @@
+const PI = 3.14;
+PI = 42;
+
+//What is the difference between var and let?
+//let variables are hoisted but not initialized, unlike var, were they are initialized as undefined
+//let variables are block-scope, meanwhile var variables are function-scoped or globally-scoped
+//let variables cannot bet redeclared in the same scope, while var allows it
+
+// What is the difference between var and const?
+// var variables can be re-assigned and re-declared, while const cannot
+
+//What is the difference between let and const?
+// you can re-assgin with let, meanwhile const doesn't let you;
+
+//What is hoisting?
+// it's how js works, where the variables and functions are moved to the top of their respectives scopres during code compilation, before the code actually runs
